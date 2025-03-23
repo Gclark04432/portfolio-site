6fa6db82a4e882b6babccc8dfa1f953989c81688
Gary Clark <gclark04432@live.co.uk>
2025-03-23 15:54:33 -0700

    Update Tailwind to newest version and implement scroll snap on y axis

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -3,18 +3,14 @@ import {
   Disclosure,
   DisclosureButton,
   DisclosurePanel,
-  Menu,
-  MenuButton,
-  MenuItem,
-  MenuItems,
 } from '@headlessui/react';
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 
 const navigation = [
-  { name: 'Dashboard', href: '#', current: true },
-  { name: 'Team', href: '#', current: false },
-  { name: 'Projects', href: '#', current: false },
-  { name: 'Calendar', href: '#', current: false },
+  { name: 'Home', href: '#home', current: true },
+  { name: 'About Me', href: '#about', current: false },
+  { name: 'Projects', href: '#projects', current: false },
+  { name: 'Contact Me', href: '#contact', current: false },
 ];
 
 function classNames(...classes: string[]) {
@@ -23,30 +19,30 @@ function classNames(...classes: string[]) {
 
 export const NavBar = () => {
   return (
-    <Disclosure as='nav' className='bg-gray-800'>
+    <Disclosure as='nav' className='absolute z-100 w-screen bg-gray-800'>
       <div className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8'>
         <div className='relative flex h-16 items-center justify-between'>
           <div className='absolute inset-y-0 left-0 flex items-center sm:hidden'>
             {/* Mobile menu button*/}
-            <DisclosureButton className='focus:outline-hidden group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:ring-2 focus:ring-inset focus:ring-white'>
+            <DisclosureButton className='group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:ring-2 focus:ring-white focus:outline-hidden focus:ring-inset'>
               <span className='absolute -inset-0.5' />
               <span className='sr-only'>Open main menu</span>
               <Bars3Icon
                 aria-hidden='true'
-                className='size-6 group-data-open:hidden block'
+                className='block w-6 group-data-open:hidden'
               />
               <XMarkIcon
                 aria-hidden='true'
-                className='size-6 group-data-open:block hidden'
+                className='hidden w-6 group-data-open:block'
               />
             </DisclosureButton>
           </div>
-          <div className='flex flex-1 items-center justify-center sm:items-stretch sm:justify-start'>
+          <div className='flex flex-1 items-center justify-end sm:items-stretch sm:justify-start'>
             <div className='flex shrink-0 items-center'>
               <img
-                alt='Your Company'
-                src='https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500'
-                className='h-8 w-auto'
+                alt='Gary Clark'
+                src='/profile.png'
+                className='h-auto w-8 rounded-full'
               />
             </div>
             <div className='hidden sm:ml-6 sm:block'>
@@ -69,61 +65,11 @@ export const NavBar = () => {
               </div>
             </div>
           </div>
-          <div className='absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0'>
-            <button
-              type='button'
-              className='focus:outline-hidden relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800'
-            >
-              <span className='absolute -inset-1.5' />
-              <span className='sr-only'>View notifications</span>
-              <BellIcon aria-hidden='true' className='w-6' />
-            </button>
-
-            {/* Profile dropdown */}
-            <Menu as='div' className='relative ml-3'>
-              <div>
-                <MenuButton className='focus:outline-hidden relative flex rounded-full bg-gray-800 text-sm focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800'>
-                  <span className='absolute -inset-1.5' />
-                  <span className='sr-only'>Open user menu</span>
-                  <img alt='' src='/profile.png' className='w-8 rounded-full' />
-                </MenuButton>
-              </div>
-              <MenuItems
-                transition
-                className='focus:outline-hidden data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black/5 transition'
-              >
-                <MenuItem>
-                  <a
-                    href='#'
-                    className='data-focus:bg-gray-100 data-focus:outline-hidden block px-4 py-2 text-sm text-gray-700'
-                  >
-                    Your Profile
-                  </a>
-                </MenuItem>
-                <MenuItem>
-                  <a
-                    href='#'
-                    className='data-focus:bg-gray-100 data-focus:outline-hidden block px-4 py-2 text-sm text-gray-700'
-                  >
-                    Settings
-                  </a>
-                </MenuItem>
-                <MenuItem>
-                  <a
-                    href='#'
-                    className='data-focus:bg-gray-100 data-focus:outline-hidden block px-4 py-2 text-sm text-gray-700'
-                  >
-                    Sign out
-                  </a>
-                </MenuItem>
-              </MenuItems>
-            </Menu>
-          </div>
         </div>
       </div>
 
       <DisclosurePanel className='sm:hidden'>
-        <div className='space-y-1 px-2 pb-3 pt-2'>
+        <div className='space-y-1 px-2 pt-2 pb-3'>
           {navigation.map((item) => (
             <DisclosureButton
               key={item.name}
