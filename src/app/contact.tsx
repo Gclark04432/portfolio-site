f4f351d068c6bbeade4ee49de36ef8eb0a26e2e2
Gary Clark <gclark04432@live.co.uk>
2025-03-26 20:30:14 -0700

    Fix: Add max width to contact me child element

--- src/app/contact.tsx
+++ src/app/contact.tsx
@@ -6,28 +6,28 @@ export const ContactMe = () => {
       id='contact'
       className='flex h-screen w-screen snap-start items-center justify-center bg-gray-800'
     >
-      <div className='box-border flex flex-wrap items-center justify-center rounded-2xl p-10'>
-        <div className='w-full max-w-sm min-w-[200px]'>
+      <div className='max-w-l box-border flex flex-wrap items-center justify-center rounded-2xl p-10'>
+        <div className='w-full min-w-[200px]'>
           <input
-            className='ease w-full rounded-md border border-slate-200 bg-transparent px-3 py-2 text-sm text-slate-700 shadow-sm transition duration-300 placeholder:text-slate-400 hover:border-slate-300 focus:border-slate-400 focus:shadow focus:outline-none'
+            className='ease w-full rounded-md border border-slate-200 bg-transparent px-3 py-2 text-sm text-slate-700 shadow-sm transition duration-600 placeholder:text-slate-400 hover:border-slate-300 focus:border-slate-400 focus:shadow focus:outline-none'
             placeholder='Your Name...'
           />
         </div>
 
-        <div className='mt-4 w-full max-w-sm min-w-[200px]'>
+        <div className='mt-4 w-full min-w-[200px]'>
           <input
-            className='ease w-full rounded-md border border-slate-200 bg-transparent px-3 py-2 text-sm text-slate-700 shadow-sm transition duration-300 placeholder:text-slate-400 hover:border-slate-300 focus:border-slate-400 focus:shadow focus:outline-none'
+            className='ease w-full rounded-md border border-slate-200 bg-transparent px-3 py-2 text-sm text-slate-700 shadow-sm transition duration-600 placeholder:text-slate-400 hover:border-slate-300 focus:border-slate-400 focus:shadow focus:outline-none'
             placeholder='Your Email...'
           />
         </div>
 
-        <div className='mt-4 w-full max-w-sm min-w-[200px]'>
+        <div className='mt-4 w-full min-w-[200px]'>
           <input
-            className='ease w-full rounded-md border border-slate-200 bg-transparent px-3 py-16 text-sm text-slate-700 shadow-sm transition duration-300 placeholder:text-slate-400 hover:border-slate-300 focus:border-slate-400 focus:shadow focus:outline-none'
+            className='ease w-full rounded-md border border-slate-200 bg-transparent px-3 py-16 text-sm text-slate-700 shadow-sm transition duration-600 placeholder:text-slate-400 hover:border-slate-300 focus:border-slate-400 focus:shadow focus:outline-none'
             placeholder='Message...'
           />
         </div>
-        <div className='mt-4 flex w-full max-w-sm min-w-[200px] items-center justify-center'>
+        <div className='mt-4 flex w-full min-w-[200px] items-center justify-center'>
           <button className='cursor-pointer rounded border border-blue-700 bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700'>
             Submit
           </button>
